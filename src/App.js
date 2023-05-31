71717bb4dfc47e03f1304946fed381f6e75ccef5
Michael Reid <EfficiencyMarriesEfficacy@gmail.com>
2023-05-30 22:35:31 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query-devtools";
+import Repositories from "./Repositories";
+
+const queryClient = new QueryClient({});
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+    
+
+      {/* The rest of your application */}
+
+      <Repositories />
+      <ReactQueryDevtools initialIsOpen={true} />
+
+    </QueryClientProvider>
+  );
+};
+
+export default App;
